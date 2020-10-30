4f1de7fed27691e241e18f9c5ba403e4734960ef
Ife Olaifa <ifeolaifa5@gmail.com>
2020-10-29 18:37:09 -0700

    added tests for getInterviewersForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -33,4 +33,25 @@ const getInterview = function(state, interview) {
 
 }
 
-export {getAppointmentsForDay, getInterview}
+const getInterviewersForDay = function(state, day) {
+
+  let emptyArray = [];
+  const filteredDay = state.days.filter(itemBooking => {
+    return itemBooking.name === day; 
+  });
+  
+  if(filteredDay.length === 0) {
+    return filteredDay;
+  } else {
+    filteredDay[0].appointments.forEach(appointmentsItem => {
+      for(let id in state.appointments) {
+        if(appointmentsItem === Number(id)) {
+          emptyArray.push(state.appointments[id]);
+        }
+      }
+    });
+  }
+  return emptyArray;
+};
+
+export {getAppointmentsForDay, getInterview, getInterviewersForDay}
